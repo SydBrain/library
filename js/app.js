92c11ebef6066b73d1571a675e981340470ba4a1
SydBrain <103507827+SydBrain@users.noreply.github.com>
2023-10-31 12:29:05 +0100

    Impleemented Book Adding logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
- document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+    // Variables
 
+    // DOM Elements
+    const addBookForm = document.querySelector('[data-form]');
+
+    addBookForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const bookData = [];
+
+        for (let value of formData.values()) {
+            bookData.push(value);
+        }
+
+        const newBook = addNewBook(...bookData);
+        console.log(newBook);
+    })
 })
+
+function Book(title, author, published, language, hasBeenRead) {
+    this.title = title;
+    this.author = author;
+    this.published = published;
+    this.language = language;
+    this.hasBeenRead = hasBeenRead;
+}
+
+function addNewBook(title, author, published, language, hasBeenRead) {
+    return new Book(title, author, published, language, hasBeenRead)
+}
