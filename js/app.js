f20321b3cad4a6c9defa5c607d31ddbe4e016ea1
SydBrain <103507827+SydBrain@users.noreply.github.com>
2023-11-24 19:00:57 +0100

    Added Language Selector + Remove Book Function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
+import { LANGUAGES } from "./constants/languages.js";
+
 document.addEventListener("DOMContentLoaded", () => {
+    // Initialization
+    populateLanguageSelector();
+
     // Variables
+    let books = [];
 
     // DOM Elements
     const addBookForm = document.querySelector('[data-form]');
+    const bookList = document.querySelector('[data-books]');
 
+    // Event Listeners
     addBookForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
@@ -14,8 +22,29 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         const newBook = addNewBook(...bookData);
-        console.log(newBook);
+        books.push(newBook);
+
+        createBookCard(newBook);
+
+        addBookForm.reset();
     })
+
+    // Functions
+    function createBookCard(book) {
+        let bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        let bookIndex = books.length - 1;
+        bookCard.setAttribute('data-book-index', bookIndex);
+
+        bookList.appendChild(bookCard);
+
+        Object.values(book).forEach(value => {
+            let bookParagraph = document.createElement('p');
+            bookCard.appendChild(bookParagraph);
+            bookParagraph.innerText = value;
+        })
+    }
 })
 
 function Book(title, author, published, language, hasBeenRead) {
@@ -29,3 +58,18 @@ function Book(title, author, published, language, hasBeenRead) {
 function addNewBook(title, author, published, language, hasBeenRead) {
     return new Book(title, author, published, language, hasBeenRead)
 }
+
+function removeBook(books, bookIndex) {
+    books.splice(bookIndex, 1);
+}
+
+function populateLanguageSelector() {
+    const languageSelect = document.querySelector('[data-languages]');
+    LANGUAGES.forEach(lang => {
+        const option = document.createElement('option');
+        option.value = lang.code;
+        option.textContent = lang.name;
+        languageSelect.appendChild(option);
+    });
+}
+
